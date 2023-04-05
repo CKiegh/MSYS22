d33afe61b7748c283e8ec7d982fb111dff029871
Kiegh <cheigh.tan@gmail.com>
2023-04-05 18:25:36 +0800

    Added Coursework

--- Lab4/MyInventorySystem/Myinventoryapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Supplier
-admin.site.register(Supplier)
-
-from .models import WaterBottle
-admin.site.register(WaterBottle)
--- Lab4/MyInventorySystem/Myinventoryapp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def hello_world(request):
-    return render(request, 'Myinventoryapp/hello_world.html')
